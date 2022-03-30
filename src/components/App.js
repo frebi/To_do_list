0766748573b63a5bda2166f6495e423fba7abc29
frebi <federico.bimi@gmail.com>
2022-03-30 15:38:45 +0200

    setup environment and basic contract routine

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Web3 from 'web3'
+
+import React, {Component} from 'react';
+import './App.css';
+
+
+class App extends Component{
+
+    render(){
+        return(
+            <div>To do list</div>
+        );
+    }
+}
+
+export default App;
