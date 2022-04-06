7a9f2b6cc8a7eeaac40930efe9b3255ea6dccbcf
frebi <federico.bimi@gmail.com>
2022-04-06 18:00:39 +0200

    working on 'completedTask' routine

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,7 @@ class App extends Component{
         //binding methods here
        //this.loadTasks = this.loadTasks.bind(this)
        this.createNewTask = this.createNewTask.bind(this)
+       this.completedTasks = this.completedTasks.bind(this)
 
     }
 
@@ -80,16 +81,21 @@ class App extends Component{
     async loadTasks(){
         for(var i=1; i<=this.state.task_count; i++){
             this.setState({task_object: await this.state.todolist.methods.tasks(i).call()})
-            this.setState({task_content: await this.state.task_object[1]})
-
-            //Updating array list without modifying the older one since the state cannot be mutated
-            this.setState(state => {
-                const content_list = this.state.content_list.concat(this.state.task_content);
-                //console.log(content_list)
-                return {
-                    content_list
-                };
-            });
+            console.log(this.state.task_object[2] + ", " + this.state.task_object[1])
+            //if(this.state.task_object[2].toString() === "false"){
+                this.setState({task_content: await this.state.task_object[1]})
+                
+                //Updating array list without modifying the older one since the state cannot be mutated
+                this.setState(state => {
+                    const content_list = this.state.content_list.concat(this.state.task_content);
+                    //console.log(content_list)
+                    return {
+                        content_list
+                    };
+                });
+            //}else{
+                //console.log("it is true" + " " + this.state.task_object[1])
+            //}
         }
     }
 
@@ -103,22 +109,45 @@ class App extends Component{
 
         this.state.todolist.methods.createTask(this.state.new_task).send({from: this.state.account})
                                     .on('transactionHash', () => {
-                                        this.addNewTask()
+                                        this.updateTaskList()
                                     })
     }
 
 
-    async addNewTask(){
+    async updateTaskList(){
         this.setState({content_list : []})
         await this.ContractInfo()
-        window.location.reload(false);
+        window.location.reload();
     }
+    
+
+
+
+    async completedTasks(elem){
+        //console.log(elem)
+        //console.log(this.state.content_list.indexOf(elem)+1)
+        if(window.confirm("The selected task will be completed")){
+            let index = this.state.content_list.indexOf(elem)+1
+            //console.log(this.state.content_list.indexOf(elem)+1)
+            this.state.todolist.methods.toggleCompleted(index).send({from : this.state.account})
+                                        .on('transactionHash', () => {
+                                            this.updateTaskList()
+                                        })
+        }else window.location.reload();
+    }
+
 
 
 
     render(){
         let tasks = this.state.content_list.map(item => (
-            <li key={item}>{item}</li>
+            
+            <div key={item} className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="task-check" onClick={() => this.completedTasks(item)}/>
+                <label className="form-check-label" htmlFor="task-check">
+                {item}
+                </label>
+          </div>
         ))
 
         return(
@@ -129,9 +158,11 @@ class App extends Component{
 									e.preventDefault()
 									this.createNewTask()
 					}}>
-                        <label>Add a new task </label><br/>
-                        <input type="text" onChange={(e) => this.setState({ new_task: e.target.value })}/>&nbsp;&nbsp;
-                        <button type="submit">Add to list</button>
+                        <div className="form-group">
+                            <label>Add a new task </label><br/>
+                            <input type="text" onChange={(e) => this.setState({ new_task: e.target.value })}/>&nbsp;&nbsp;
+                            <button type="submit">Add to list</button>
+                        </div>
                     </form>
                 </div><br/>
                 {this.state.content_list.length !== 0 ? 
